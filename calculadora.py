91244b7a48ed34220ec337e6ee2790844919eaf0
Bielz <gabriel.rocha.silva777@gmail.com>
2024-10-18 16:56:46 -0300

    Add files via upload
    minha primeira calculadora inteligente no Python!

---
+++ calculadora.py
@@ -0,0 +1,27 @@
+ramdom = 'digite um numero'
+xd = 'parabens'
+queijo = 'por ter completado o circuito das 4 operações! até logo!'
+nome = str(input('digite seu nome'))
+print(nome,ramdom)
+print('estamos na adição')
+n1 = float(input(':'))
+n2 = float(input(':'))
+soma = n1 + n2
+print(soma)
+print('agora iremos para a subtração.')
+print('digite um numero')
+n3 = float(input(':'))
+n4 = float(input(':'))
+subtracao = n3 - n4
+print(subtracao)
+print('iremos para a multiplicação agora')
+n5 = float(input(':'))
+n6 = float(input(':'))
+multiplicacao = n5 * n6
+print(multiplicacao)
+print('e pro fim, terminamos com a divisão')
+n7 = float(input(':'))
+n8 = float(input(':'))
+divisao = n7 / n8
+print(divisao)
+print(xd,nome,queijo)
